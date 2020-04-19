21fdd188e0f2e2409c47fad1eda0e0c783a2a50e
Jadw1 <jadw1_1@o2.pl>
2020-04-19 22:30:52 +0200

    torch

---
+++ Assets/Game Objects/Character/Torch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Torch : MonoBehaviour {
+    public int maxCondition;
+    public int condition;
+
+    public float maxRadious;
+    
+    private Light2D light;
+    
+    // Start is called before the first frame update
+    void Start() {
+        light = GetComponent<Light2D>();
+        GameObject.FindWithTag("Game Master").GetComponent<GameManager>().tickEvent += OnTick;
+        
+        SetTorch(maxCondition, maxRadious);
+    }
+
+    public void SetTorch(int maxCondition, float maxRadious) {
+        this.maxCondition = maxCondition;
+        this.maxRadious = maxRadious;
+
+        condition = maxCondition;
+        light.pointLightOuterRadius = maxRadious;
+    }
+
+    void OnTick(int tick) {
+        if (condition > 0) {
+            condition--;
+            light.pointLightOuterRadius = Mathf.Lerp(0.0f, maxRadious, (float) condition / maxCondition);
+        }
+        //event if light goes out?
+    }
+}
