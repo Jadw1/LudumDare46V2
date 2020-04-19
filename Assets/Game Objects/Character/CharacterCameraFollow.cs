5a1767fdb1cee18e03b1184d2cf1c586c170a51c
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-19 17:01:05 +0200

    More progress on the map

---
+++ Assets/Game Objects/Character/CharacterCameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCameraFollow : MonoBehaviour
+{
+    public float lerpFactor = 0.8f;
+    private Transform _obj;
+
+    private void Start()
+    {
+        _obj = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        var pos = transform.localPosition;
+
+        var destPos = Vector3.Lerp(pos, _obj.position, lerpFactor);
+        destPos.z = pos.z;
+
+        transform.localPosition = destPos;
+    }
+}
