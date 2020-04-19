2af6d69cb67f3e6b47857745e35e58ee25e469b0
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-19 15:29:01 +0200

    Add tile entity class

---
+++ Assets/Game Objects/Tile Entity/TileEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEntity : MonoBehaviour
+{
+    public Vector2 GetPos()
+    {
+        Vector3 localPosition = transform.localPosition;
+        return new Vector2(localPosition.x, localPosition.y);
+    }
+
+    public void Move(int x, int y)
+    {
+        transform.localPosition = new Vector3(x, y, 0);
+    }
+}
