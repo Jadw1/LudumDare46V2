a60bb8663399ebe01c5d6010a7034b0ff65dc9c6
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-19 19:51:11 +0200

    Add fireplace

---
+++ Assets/Game Objects/Character/CharacterInventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LightSource
+{
+    MATCH,
+    TORCH
+}
+
+public enum InventoryItem
+{
+    PICKAXE
+}
+
+public class CharacterInventory : MonoBehaviour
+{
+    
+}
---
+++ Assets/Game Objects/Fireplace/Fireplace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    private ParticleSystem _particleSystem;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        
+        _particleSystem.Stop();
+        _spriteRenderer.color = Color.white;
+    }
+}
