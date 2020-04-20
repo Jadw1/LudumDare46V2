71a90469ff32d5a14ab6fba6861efe0cbe4f9395
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-20 15:38:52 +0200

    Improve bushes

---
+++ Assets/Game Objects/Tile Entity/Bush.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : DestructableEntity
+{
+    protected override void OnTryBreak(CharacterManager characterManager)
+    {
+        characterManager.ThinkImmediete("[try again to break this]");
+    }
+}
