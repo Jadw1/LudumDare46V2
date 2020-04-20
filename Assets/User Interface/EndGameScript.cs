18b623b2481e1f0ec03697a67c0111c1a5fb75c9
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-20 19:09:41 +0200

    Add end game screen

---
+++ Assets/User Interface/EndGameScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameScript : MonoBehaviour
+{
+    private bool loading = false;
+    private void Update()
+    {
+        if (!loading && Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+    
+    IEnumerator LoadYourAsyncScene()
+    {
+        loading = true;
+        var asyncLoad = SceneManager.LoadSceneAsync("MainMenuScene");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
