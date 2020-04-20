243a063f11fffc7f1020557ebdaf0ab1405c5f2d
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-04-20 10:18:25 +0200

    Add main menu

---
+++ Assets/User Interface/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private Slider _slider;
+
+    private void DisableSlider()
+    {
+        _slider.enabled = false;
+
+        foreach (var image in _slider.GetComponentsInChildren<Image>())
+        {
+            image.enabled = false;
+        }
+    }
+    
+    private void EnableSlider()
+    {
+        _slider.enabled = true;
+
+        foreach (var image in _slider.GetComponentsInChildren<Image>())
+        {
+            image.enabled = true;
+        }
+    }
+
+    private void Start()
+    {
+        _slider = GetComponentInChildren<Slider>();
+        DisableSlider();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        EnableSlider();
+        
+        var asyncLoad = SceneManager.LoadSceneAsync("GameScene");
+
+        while (!asyncLoad.isDone)
+        {
+            _slider.value = asyncLoad.progress;
+            yield return null;
+        }
+    }
+}
